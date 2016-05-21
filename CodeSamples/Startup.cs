6655bf32636d9d9ada0e92e9069843ee06fe706f
pawan <pawan@PAWANPRAKASHPAL>
2016-05-21 10:42:59 -0400

    Add project files.

---
+++ CodeSamples/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeSamples.Startup))]
+namespace CodeSamples
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
